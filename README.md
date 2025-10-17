f566315f22ae7167cc68b59a7efed83575752cb6
MoniqueTheogene123 <716theogene@gmail.com>
2025-10-17 11:53:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<!-- TODO: copy readme template -->
+# TicTacToe
+
+
+## Run App
+
+`python3 app.py`
---
+++ app.py
@@ -0,0 +1,26 @@
+# app.py
+
+def main():
+    print("Welcome to TicTacToe")
+    
+    # setup board
+    board = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    
+    # print instructions
+
+    # start game loop
+    
+        # get user input
+        
+        # update board
+
+        # if there is a winner/loser
+            # print outcome
+            # exit
+
+if __name__ == "__main__":
+    main()
